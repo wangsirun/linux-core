eabaf706c89ef063706ae43afd344d844dc29722
wangsirun <wangsirun@zhidaoauto.com>
2025-05-19 14:41:06 +0800

    add compile_commands.json

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP */
+#define UTS_MACHINE "aarch64"
+#define UTS_VERSION "#1 SMP Thu Nov 21 01:43:27 CST 2024"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "7476a772d39e"
+#define LINUX_COMPILER "gcc version 9.4.0 (Ubuntu 9.4.0-1ubuntu1~20.04.1)"
